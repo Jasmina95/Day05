1e6433ca1931ee175b958e3f17415def2697e4e3
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-12 23:45:33 +0100

    Concatenating Strings with Plus Operator

---
+++ ex04/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    //Only change code below this line
+    var myString = "This is the first String. " + "This is the second String.";
+    //Only change code above this line 
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
