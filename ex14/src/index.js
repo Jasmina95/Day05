8d22b09c0ea78e842d301caf1b76011bea4a4015
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-12 23:52:48 +0100

    Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "His " + myNoun + " " + myVerb + " away, after sensing that " + myAdjective + " storm was approaching, very " + myAdverb + ".";
+    //Only change code above this line 
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
